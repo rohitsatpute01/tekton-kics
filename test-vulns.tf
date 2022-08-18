e9a9a9c158005aa9b61606d9b80ba76f650d09da
rohitsatpute01 <111521051+rohitsatpute01@users.noreply.github.com>
2022-08-18 21:02:31 +0530

    Update test-vulns.tf

--- test-vulns.tf
+++ test-vulns.tf
@@ -9,16 +9,3 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
-
